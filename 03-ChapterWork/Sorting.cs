25b286dd79e211de78fbe63ebd231d02b9970d40
Synopsik <synopsikk@gmail.com>
2025-06-23 22:19:15 -0700

    Created stopwatch, Insertion Sort, and Bubble Sort

--- 03-ChapterWork/Sorting.cs
+++ 03-ChapterWork/Sorting.cs
@@ -1,19 +1,38 @@
+using System.Diagnostics;
+
 namespace Chapter3;
 
 public class Sorting
 {
     public static void Demo()
     {
+        Console.WriteLine("\n\n");
         Random random = new Random();
         int[] array = ResetArray();
+        var sw = Stopwatch.StartNew();
+        sw.Stop();
+        
+        Console.WriteLine("Selection Sort");
+        sw.Restart();
         SelectionSort(array);
-        Console.WriteLine(string.Join(" | ", array));
+        sw.Stop();
+        Console.WriteLine($"Time Elapsed: {sw.Elapsed.TotalMilliseconds} ms");
+        
         array = ResetArray();
 
+        Console.WriteLine("\nInsertion Sort");
+        sw.Restart();
         InsertionSort(array);
-        Console.WriteLine(string.Join(" | ", array));
+        sw.Stop();
+        Console.WriteLine($"Time Elapsed: {sw.Elapsed.TotalMilliseconds} ms");
+        
         array = ResetArray();
-
+        
+        Console.WriteLine("\nBubble Sort");
+        sw.Restart();
+        BubbleSort(array);
+        sw.Stop();
+        Console.WriteLine($"Time Elapsed: {sw.Elapsed.TotalMilliseconds} ms");
     }
 
     
@@ -24,6 +43,7 @@ public class Sorting
     {
         for (int i = 0; i < a.Length - 1; i++)
         {
+            // Console.WriteLine(string.Join(" | ", a));
             int minIndex = i;
             int minValue = a[i];
             for (int j = i + 1; j < a.Length; j++)
@@ -41,7 +61,33 @@ public class Sorting
 
     public static void InsertionSort(int[] a)
     {
-        
+        for (int i = 1; i < a.Length; i++)
+        {
+            // Console.WriteLine(string.Join(" | ", a));
+            int j = i;
+            while (j > 0 && a[j] < a[j - 1])
+            {
+                (a[j], a[j - 1]) = (a[j - 1], a[j]);
+                j--;
+            }
+        }
+    }
+
+    public static void BubbleSort(int[] a)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            bool isAnyChange = false;
+            for (int j = 0; j < a.Length - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    isAnyChange = true;
+                    (a[j], a[j + 1]) = (a[j + 1], a[j]);
+                }
+            }
+            if (!isAnyChange){break;}
+        }
     }
     
 }
