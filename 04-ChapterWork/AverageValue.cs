d4c5d8cf1c8c06669026b4d3dcf8e79e857e4c5b
Synopsik <synopsikk@gmail.com>
2025-06-24 22:20:05 -0700

    Synopsik: Update RootNamespace and include Demo for AverageValue

--- 04-ChapterWork/AverageValue.cs
+++ 04-ChapterWork/AverageValue.cs
@@ -1,6 +1,17 @@
-namespace _04_ChapterWork;
+namespace Chapter4;
 
 public class AverageValue
 {
-    
+    public static void Demo()
+    {
+        List<double> num = [];
+        do
+        {
+            Console.Write("Enter the number: ");
+            string numStr = Console.ReadLine() ?? string.Empty;
+            if(!double.TryParse(numStr, out double n)) {break;}
+            num.Add(n);
+            Console.WriteLine($"Average value: {num.Average()}");            
+        } while (true);
+    }
 }
