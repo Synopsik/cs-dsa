9d4a832e9c91093e39f504d8030e80e63e7e1533
Synopsik <synopsikk@gmail.com>
2025-06-25 15:01:33 -0700

    Synopsik: Including examples from hanoi and call center

--- 05-ChapterWork/Program.cs
+++ 05-ChapterWork/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Chapter5;
 
-Console.WriteLine("Hello, World!");
+/*
+/ ------------------------------------------------------------ /
+/                     Tower of Hanoi example                   /
+/ ------------------------------------------------------------ /
+/
+Game game = new(5);
+Visualization vis = new(game);
+game.MoveCompleted += (s, e) => vis.Show((Game)s!);
+await game.MoveAsync(game.DiscsCount, game.From, game.To, game.Auxiliary);
+*/
+
+
+
+Random random = new();
+
+CallCenter center = new();
+center.Call(1234);
+center.Call(5678);
+center.Call(1468);
+center.Call(9641);
+
+void Log(string text) => Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {text}");
+
+while (center.AreWaitingCalls())
+{
+    IncomingCall call = center.Answer("Marcin")!;
+    
+    Log($"Call #{call.Id} from client " +
+            $"#{call.ClientID} answered by {call.Consultant}.");
+    
+    await Task.Delay(random.Next(1000, 10000));
+    
+    center.End(call);
+    
+    Log($"Call #{call.Id} from client " +
+            $"#{call.ClientID} ended by {call.Consultant}");
+}
+
