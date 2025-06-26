2fa968258b8db2b0c2dc4c56c8a760763743ae3e
Synopsik <synopsikk@gmail.com>
2025-06-26 10:43:06 -0700

    Synopsik: Finished CircularCoasterDemo

--- 05-ChapterWork/CircularQueue.cs
+++ 05-ChapterWork/CircularQueue.cs
@@ -1,3 +1,4 @@
+using QueueItem = (System.DateTime StartedAt, System.ConsoleColor Color);
 namespace Chapter5;
 
 public class CircularQueue<T>(int size) 
@@ -59,8 +60,50 @@ public class CircularQueue<T>(int size)
         }
     }
 
-    public static void CircularCoasterDemo()
+    public static async Task CircularCoasterDemo()
     {
-        
+        const int rideSeconds = 10;
+        Random random = new();
+        CircularQueue<QueueItem> queue = new(12);
+        ConsoleColor color = ConsoleColor.Black;
+
+        while (true)
+        {
+            while (queue.Peek() != null)
+            {
+                QueueItem item = queue.Peek()!.Value;
+                TimeSpan elapsed = DateTime.Now - item.StartedAt;
+                if (elapsed.TotalSeconds < rideSeconds) {break;}
+
+                queue.Dequeue();
+                Log($"> Exits\tTotal: {queue.Count}", item.Color);
+            }
+
+            bool isNew = random.Next(3) == 1;
+            if (isNew)
+            {
+                color = color == ConsoleColor.White
+                    ? ConsoleColor.DarkBlue
+                    : (ConsoleColor)(((int)color) + 1);
+                if (queue.Enqueue((DateTime.Now, color)))
+                {
+                    Log($"< Enters\tTotal: {queue.Count}", color);
+                }
+                else
+                {
+                    Log($"! Not allowed\tTotal: {queue.Count}",
+                        ConsoleColor.DarkGray);
+                }
+            }
+
+            await Task.Delay(500);
+        }
+    }
+
+    private static void Log(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} {text}");
+        Console.ResetColor();
     }
 }
