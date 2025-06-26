033aa1f310beb36eb99fe28a0a22ca01ea214c3a
Synopsik <synopsikk@gmail.com>
2025-06-25 22:54:15 -0700

    Synopsik: Simplifying events to be contained within their class file

--- 05-ChapterWork/Program.cs
+++ 05-ChapterWork/Program.cs
@@ -5,7 +5,12 @@
 /                     Tower of Hanoi example                   /
 / ------------------------------------------------------------ /
 */
-Game game = new(5);
-Visualization vis = new(game);
-game.MoveCompleted += (s, e) => vis.Show((Game)s!);
-await game.MoveAsync(game.DiscsCount, game.From, game.To, game.Auxiliary);
+// await Game.Demo();
+
+
+/*
+/ ------------------------------------------------------------ /
+/                       Queue Examples                         /
+/ ------------------------------------------------------------ /
+*/
+await Queue.Demo();
